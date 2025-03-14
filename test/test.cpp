af8156d42351d724b784d71f23638d34cf323629
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2025-03-14 17:28:08 +0100

    Improve test suite

--- test/test.cpp
+++ test/test.cpp
@@ -7,33 +7,32 @@
 #define HEIGHT 800
 #define WIDTH  800
 
-#define RETURN_DEFER(fd, canvas, status_code)       \
+#define RETURN_DEFER(fd, status_code)               \
     save_to_ppm(fd, canvas);                        \
     fd.close();                                     \
     return status_code
 
 #define RUN_TEST(test_function, file) do {          \
-    if (test_function(pixels, file)) {              \
+    if (test_function(file)) {                      \
         std::cerr << "Error with " << file << "\n"; \
         return 1;                                   \
     }                                               \
 } while (0)
 
-auto test_circle(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
+static uint32_t pixels[HEIGHT * WIDTH];
+static Canvas   canvas {pixels, HEIGHT, WIDTH};
 
+auto test_circle(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
     fill(canvas, 0xFF000000);
     shapes::circle(canvas, 400, 400, 100.0, 0xFFFF0000);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_copy(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_copy(const std::string file_name) -> int {
     std::ifstream input_file {file_name, std::ios::in};
     if (!input_file) return 1;
 
@@ -43,12 +42,10 @@ auto test_copy(uint32_t *pixels, const std::string file_name) -> int {
     std::ofstream output_file {"./outputs/copy.ppm", std::ios::out};
     if (!output_file) return 1;
     
-    RETURN_DEFER(output_file, canvas, 0);
+    RETURN_DEFER(output_file, 0);
 }
 
-auto test_flip_rectangle(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_flip_rectangle(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
@@ -56,24 +53,20 @@ auto test_flip_rectangle(uint32_t *pixels, const std::string file_name) -> int {
     shapes::rectangle(canvas, 150, 200, 100, 200, 0xFFA1FF15);
     effects::flip_ppm(canvas);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_line(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_line(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
     fill(canvas, 0xFF000000);
     shapes::line(canvas, 0, 0, canvas.width, canvas.height, 0xFFFF00FF);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_overlap(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_overlap(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
@@ -82,12 +75,10 @@ auto test_overlap(uint32_t *pixels, const std::string file_name) -> int {
     shapes::circle(canvas, 400, 400, 100, 0xFFF0000);
     shapes::circle(canvas, 300, 300, 75, 0xFF0000FF, 0.5);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_rotate_rectangle(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_rotate_rectangle(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
@@ -95,36 +86,30 @@ auto test_rotate_rectangle(uint32_t *pixels, const std::string file_name) -> int
     shapes::rectangle(canvas, 150, 200, 100, 200, 0xFFA1FF15);
     effects::rotate_ppm(canvas);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_text(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_text(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
     fill(canvas, 0xFF000000);
     shapes::text(canvas, std::string{"hello, world!"}, 150, 200, 10, 0xFF00FF00, default_font);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
-auto test_triangle(uint32_t *pixels, const std::string file_name) -> int {
-    Canvas canvas {pixels, HEIGHT, WIDTH};
-
+auto test_triangle(const std::string file_name) -> int {
     std::ofstream fd {file_name, std::ios::out};
     if (!fd) return 1;
 
     fill(canvas, 0xFF000000);
     shapes::triangle(canvas, 100, 100, 600, 200, 400, 500, 0xFFEE00FF);
 
-    RETURN_DEFER(fd, canvas, 0);
+    RETURN_DEFER(fd, 0);
 }
 
 auto main() -> int {
-    uint32_t pixels[HEIGHT * WIDTH];
-
     RUN_TEST(test_circle, "./outputs/circle.ppm");
     RUN_TEST(test_copy, "./outputs/circle.ppm");
     RUN_TEST(test_flip_rectangle, "./outputs/flipped_rectangle.ppm");
